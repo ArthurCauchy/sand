72c843245d7a4e220f869d04e10128cee70d4ca7
ArthurCauchy <arthur.cauchy@free.fr>
2018-12-06 21:21:32 +0100

    initial commit

--- Main.gd
+++ Main.gd
@@ -1,28 +1,29 @@
 extends Spatial
 
 var height = 0
-var lamp_moving = false
-var time = 0
 
 func _ready():
 	$Camera.look_at($Terrain.translation, Vector3(0, 1, 0))
-	$UI/ColorPickerButton.color = $Terrain.color
-
-func _process(delta):
-	if lamp_moving:
-		time += delta
-		$Lamp.translation.x = 20 * sin(time);
 
 func _input(event):
-	# toggle lamp movement
-	if event.is_action_pressed("ui_accept"):
-		lamp_moving = not lamp_moving
+	# toggle fullscreen
+	if event.is_action_pressed("ui_fullscreen"):
+    	OS.window_fullscreen = !OS.window_fullscreen
+	# move light
+	if event.is_action_pressed("ui_left"):
+		$Lamp.translation.x -= 1
+	elif event.is_action_pressed("ui_right"):
+		$Lamp.translation.x += 1
+	elif event.is_action_pressed("ui_up"):
+		$Lamp.translation.z -= 1
+	elif event.is_action_pressed("ui_down"):
+		$Lamp.translation.z += 1
 	# increase terrain height
-	if event.is_action_pressed("ui_up") and not event.is_echo() and height <= 9:
+	if event.is_action_pressed("action_heightup") and height < 15:
 		height += 1
 		$Terrain.get_surface_material(0).set_shader_param("height_range", height)
 	# decrease terrain height
-	elif event.is_action_pressed("ui_down") and not event.is_echo() and height >= 1:
+	elif event.is_action_pressed("action_heightdown") and height > 0:
 		height -= 1
 		$Terrain.get_surface_material(0).set_shader_param("height_range", height)
 	$UI/HeightFactor.text = "Height Factor: " + str(height)
